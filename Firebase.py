19cadfe4c12e00d83c349798ca08f702ef79eecd
jurtomasz13 <jur.tomasz13@gmail.com>
2022-06-03 13:21:13 +0200

    Deleted comments from Firebase.py

--- Firebase.py
+++ Firebase.py
@@ -10,11 +10,3 @@ config = {
 firebase = pyrebase.initialize_app(config)
 db = firebase.database()
 auth = firebase.auth()
-
-#   apiKey: "AIzaSyAVscr9cvbOXD-AtB4vt9tzwBDjj0v7e48",
-#   authDomain: "python-rpg-mirumee-starter.firebaseapp.com",
-#   databaseURL: "https://python-rpg-mirumee-starter-default-rtdb.europe-west1.firebasedatabase.app",
-#   projectId: "python-rpg-mirumee-starter",
-#   storageBucket: "python-rpg-mirumee-starter.appspot.com",
-#   messagingSenderId: "644731220342",
-#   appId: "1:644731220342:web:53b89284341b8d10a50a57"
